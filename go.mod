3489d5ea6e189514eb6fe82ac04780ed00105acb
xiaojingren <835505371@qq.com>
2020-08-06 11:32:45 +0800

    add a function DoSomething()

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Xezeloh/my-go-mod-demo
+
+go 1.14
---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func DoSomething(name string) {
+	fmt.Println(name, ", welcome!")
+}
+
+func main() {
+	DoSomething("Nvwushen")
+}
